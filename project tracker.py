633f285f958967b613b8c9df5e0541b8888ed1a5
Gurupreet Kumar <gurupreetkumar2008@gmail.com>
2025-06-24 20:14:39 +0530

    Edited the exit block

--- project tracker.py
+++ project tracker.py
@@ -26,31 +26,44 @@ for Index, target in enumerate(targets, 1):
     iterated_target = print(f"{Index}. {target}")
 
 while True:
-    indexing = 0
-    print("Let's do:\nTarget: ")
-
     # Focus Timer Input
+    print("NOTE: Type done or exit to exit the timer.")
     print("\nEnter total focus time in minutes:")
 
-    focus_time = int(input("Focus time: "))
-    # Pomodoro Loop
-    while focus_time >= 25:
-        print("\nFocus session started (25 min)")
-        timer(25)
-        focus_time -= 25
-        print("Focus session ended. Time for a 5 min break!\n")
-        
-        if focus_time >= 5:
-            timer(5)
-            focus_time -= 5
-            print("Break over! Get ready for next focus session.\n")
-        else:
-            print("Not enough time left for a full break. Ending session.")
+    focus_time = input("Focus time: ")
+    try:
+        str_focus_time = str(focus_time)
+        if str_focus_time.lower() == "done" or str_focus_time.lower() == "exit" :
             break
+        else:
+            raise ValueError("Enter the valid value.")
 
-    # Final remaining time
-    if 0 < focus_time < 25:
-        print(f"\nThis is short focus session for {focus_time} minutes.")
-        timer(focus_time)
+    except:
+        pass
+    
+    try:
+        focus_time = int(focus_time)
+
+        # Pomodoro Loop
+        while focus_time >= 25:
+            print("\nFocus session started (25 min)")
+            timer(25)
+            focus_time -= 25
+            print("Focus session ended. Time for a 5 min break!\n")
+            
+            if focus_time >= 5:
+                timer(5)
+                focus_time -= 5
+                print("Break over! Get ready for next focus session.\n")
+            else:
+                print("Not enough time left for a full break. Ending session.")
+                break
+
+        # Final remaining time
+        if 0 < focus_time < 25:
+            print(f"\nThis is short focus session for {focus_time} minutes.")
+            timer(focus_time)
+    except ValueError:
+        print("Please enter a valid number for focus time.")
     
 
